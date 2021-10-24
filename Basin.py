5886288bbea3cbedc663e292d5c00d5d5c949348
AvigayilBrown <avigayil.wimpfheimer@mail.huji.ac.il>
2021-10-24 15:02:07 +0300

    checking git

--- Basin.py
+++ Basin.py
@@ -1,3 +1,22 @@
+import numpy as np
+from sklearn.linear_model import LinearRegression
+import matplotlib.pyplot as plt
+import pandas as pd
+from sklearn.model_selection import train_test_split
+from datetime import datetime
+from pandas import DataFrame
+import matplotlib.dates as dates
+from matplotlib.dates import DateFormatter
+""" todo: 
+timestamps
+create X,y
+split train, val, pred
+make model
+make plot"""
+
+HEADER = ['station', 'year', 'month', 'day', 'hour', 'is_row_bad', 'Level', 'Battery']
+
+
 class Basin:
     """
     This class represents a single basin. 
@@ -6,7 +25,8 @@ class Basin:
     # counts the number of basins
     counter = 0
 
-    def __init__(self, id,stations_info, file_flow):
+    def __init__(self, id, file_flow, stations_info=None):
+        self.features = {}
         Basin.counter += 1
 
         self.id = id
@@ -19,18 +39,128 @@ class Basin:
         self.area = None
         self.latitude = None  # m
         self.elevation = None  # m
-        
-        # dataframe with all basin data
-        self.data = None
 
+        self.data = None
+        self.X = None  # features
+        self.y = None  # labels
+        self.f = {}  # features
+        self.timestamp = None
 
     def process_station_info(self):
         pass
+
     # put the area for each basin
 
     def process_flow_file(self):
-        pass
-    
-    def model(self):
-        pass
+        df = pd.read_csv(self.f_flow, sep='\t',
+                         names=HEADER, parse_dates={'date': ['year', 'month', 'day', 'hour']})
+        df = df.dropna(axis=0)
+        self.data = df
 
+    def split_data(self):
+        """
+        split data into training, validation and test sets
+        """
+        # todo add validation split, and add to features
+        # todo talk about size of split (ratio)
+        X_train, X_test, y_train, y_test = train_test_split(self.X, self.y, test_size=0.2, random_state=1)
+        # X_train, X_val, y_train, y_val= train_test_split(X_train, y_train, test_size=0.25, random_state=1)  # 0.25 x 0.8 = 0.2
+
+        split = round(self.data.shape[0] // 78*(0.8))
+        X_train,X_test = np.split(self.X,[split],axis=0)
+        y_train,y_test = np.split(self.y,[split],axis=0)
+        timestamp_train, self.timestamp = np.split(self.timestamp,[split],axis=0)
+        self.f = {"X_train": X_train, "y_train": y_train,
+                  "X_test": X_test, "y_test": y_test}
+
+    def create_features(self):
+        """
+        feature - 72 hours of streamflow data
+        label - streamflow in 6 hours
+        """
+        d = 72
+        lag = 5  # 6 hours ahead
+        m = self.data.shape[0] // 78
+        X = np.zeros((m, d))
+        flow = self.data['Level']
+
+        for i in range(m):
+            X[i] = flow.iloc[i:i + d]
+        y = flow[d + lag:d + m + lag].reset_index(drop=True)
+        self.timestamp = self.data['date'][d + lag:d + m + lag].reset_index(drop=True)
+        self.X = X
+        self.y = y
+
+
+
+    def model(self):
+        """
+        regression model for stream prediction.
+        """
+        # regularization: minus min divide by difference max min
+        X_train = self.f['X_train']
+        X_min,X_max = X_train.min(), X_train.max()
+        X_train = (X_train - X_min) / (X_max - X_min)
+
+        y_train = self.f['y_train']
+        y_min, y_max = y_train.min(), y_train.max()
+        y_train = (y_train - y_min) / (y_max - y_min)
+
+        # linear regression
+        model = LinearRegression()
+        model.fit(X_train, y_train)
+
+        X_test = (self.f['X_test'] - X_min) / (X_max - X_min)
+        y_pred = model.predict(X_test)
+        y_pred = y_pred * (y_max - y_min) + y_min
+        return y_pred
+
+    def NSE(self, y_pred, y_test):
+        """
+        uses NSE as the prediction assessment for model
+        """
+        # todo fix function
+        error_variance = np.sum(np.power((y_pred - y_test), 2))
+        variance = np.sum(np.power((y_test - np.mean(y_test)), 2))
+        NSE = 1 - (error_variance / variance)
+        return NSE
+
+    def asses_models(self, y_pred):
+        """
+        assessing model performance using NSE coefficient.
+        """
+        # unregularize
+
+        # with open(self.nse_file, 'a', newline='') as f:
+        #     writer = csv.writer(f, delimiter=',')
+        #     if Basin.counter == 1:
+        #         writer.writerow(["NSE"])
+        #     writer.writerow([str(self.NSE(y_pred, self.y_test))])
+        print(self.NSE(y_pred, self.f['y_test']))
+
+    def plot_prediction(self, y_pred):
+        """
+        plots the streamflow prediction vs reality per day.
+        """
+
+        fig, ax = plt.subplots()
+        plt.title(label="Predicted and actual streamflow per day")
+        ax.plot(self.timestamp,self.f['y_test'], label="actual")
+        ax.set_xlabel("year")
+        ax.set_ylabel("streamflow mm/day")
+        ax.plot(self.timestamp,y_pred, label="predicted")
+        ax.legend()
+        plt.show()
+
+if __name__ == '__main__':
+    pass
+    file_flow = "C:\\Users\\asus\\Desktop\\Israel_flood_prediction\\data_14120.csv"
+    stations_id = "data//stations.xlsx"
+    station_info = "data//קטלוג_תחנות_הידרומטריות.xlsx"
+    b = Basin(id, file_flow)
+    b.process_flow_file()
+    b.create_features()
+    b.split_data()
+    y_pred = b.model()
+    b.asses_models(y_pred)
+    # b.plot_prediction(y_pred)
--- main.py
+++ main.py
@@ -2,8 +2,13 @@ import csv
 import pandas as pd
 from Basin import Basin
 
+TIME = {100: '01', 200: '02', 300: '03', 400: '04', 500: '05', 600: '06', 700: '07'
+    , 800: '08', 900: '09', 1000: '10', 1100: '11', 1200: '12', 1300: '13', 1400: '14'
+    , 1500: '15', 1600: '16', 1700: '17', 1800: '18', 1900: '19', 2000: '20'
+    , 2100: '21', 2200: '22', 2300: '23', 2400: '0'}
 
-def process_data(stations_id, stations_info, metatok):
+
+def process_data(stations_id, metatok):
     """
     extract data for each one of selected basins to csv file
     """
@@ -12,30 +17,38 @@ def process_data(stations_id, stations_info, metatok):
     id = df['ID'].to_list()
     selected_basins = {}
     header = ['station', 'year', 'month', 'day', 'time', 'is_row_bad', 'Level', 'Battery']
-    print(id)
     for i in id:
         file_name = 'data_' + str(i) + '.csv'
         selected_basins[i] = file_name
 
-        with open(file_name, 'w',newline='') as f:
-            writer = csv.writer(f)
-            writer.writerow(header)
+        # with open(file_name, 'w',newline='') as f:
+        #     writer = csv.writer(f)
+        #     writer.writerow(header)
 
-        for file in metatok:
-            df = pd.read_csv(file)
-            if i in df['station'].values:
-                df2 = df[df['station'] == i]
-                print(str(i) + " in " + str(file_name))
-                print(df2)
-                df2.to_csv(file_name, mode='a', sep=',', index=False, header=False)
+        # for file in metatok:
+        #     df = pd.read_csv(file)
+        #     if i in df['station'].values:
+        #         df = df[df['station'] == i]
+        #         df = df[df['time'] % 100 == 0] # takeout not round hours
+        #         df = df.replace({'time': TIME})
+        #         df['time'] = df['time'].astype(int)
+        #
+        #         # df['date'] = pd.to_datetime(df['year'] * 10000 + df['month'] * 100 + df['day'], format='%Y%m%d')
+        #         df.to_csv(file_name, mode='a', sep='\t', index=False, header=False)
     return selected_basins
 
 
 def create_basins(selected_basins, stations_info):
     for id, file_flow in selected_basins.items():
-        Basin(id, file_flow, stations_info)
-    print(Basin.counter)
-
+        b = Basin(id, file_flow, stations_info)
+        b.process_flow_file()
+        b = Basin(id, file_flow)
+        b.process_flow_file()
+        b.create_features()
+        b.split_data()
+        y_pred = b.model()
+        b.asses_models(y_pred)
+        b.plot_prediction(y_pred)
 
 if __name__ == '__main__':
     # general station files
@@ -49,5 +62,5 @@ if __name__ == '__main__':
     metatok_4 = "data//ihs_1hr_2019__2021.csv"
     metatok = [metatok_1, metatok_2, metatok_3, metatok_4]
 
-    selected_basins = process_data(stations_id, station_info, metatok)
-    # create_basins(selected_basins, station_info)
+    selected_basins = process_data(stations_id, metatok)
+    create_basins(selected_basins, station_info)
